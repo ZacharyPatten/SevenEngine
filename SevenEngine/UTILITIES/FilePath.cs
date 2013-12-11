27df0402dd02b03a70345486c53022d73367e153
53V3N1X <sevenix.zp@gmail.com>
2013-12-10 23:15:27 -0600

    FilePath fix

--- SevenEngine/UTILITIES/FilePath.cs
+++ SevenEngine/UTILITIES/FilePath.cs
@@ -18,14 +18,18 @@ namespace SevenEngine
   /// <summary>Will generate filepaths in a cross platform manor.</summary>
   public static class FilePath
   {
+    /// <summary>Takes a string representing a file path with folder delimiters '\' or '/' 
+    /// and puts in the correct delimiter based on the OS.</summary>
+    /// <param name="path">The path to make cross platform.</param>
+    /// <returns>The correctly formatted filepath string.</returns>
     public static string FromRelative(string path)
     {
       if ((path[0] == '\\' || path[0] == '/') && (path[path.Length - 1] == '\\' || path[path.Length - 1] == '/'))
-        return Directory.GetCurrentDirectory() + Path.PathSeparator + Path.Combine(path.Split('\\')) + Path.PathSeparator;
+        return Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + Path.Combine(path.Split('\\')) + Path.DirectorySeparatorChar;
       else if (path[0] == '\\' || path[0] == '/')
-        return Directory.GetCurrentDirectory() + Path.PathSeparator + Path.Combine(path.Split('\\'));
+        return Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + Path.Combine(path.Split('\\'));
       else if (path[path.Length - 1] == '\\' || path[path.Length - 1] == '/')
-        return Directory.GetCurrentDirectory() + Path.Combine(path.Split('\\')) + Path.PathSeparator;
+        return Directory.GetCurrentDirectory() + Path.Combine(path.Split('\\')) + Path.DirectorySeparatorChar;
       else
         return Directory.GetCurrentDirectory() + Path.Combine(path.Split('\\'));
     }
