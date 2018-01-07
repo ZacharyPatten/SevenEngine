fcf46379662db1bd550916aa0524ea55e4be210c
Zachary Patten <sevenix.zp@gmail.com>
2018-01-06 20:54:55 -0600

    compile fix

--- SevenEngine/Graphics/Graphics.cs
+++ SevenEngine/Graphics/Graphics.cs
@@ -1,46 +1,46 @@
-﻿// Seven
-// https://github.com/53V3N1X/SevenEngine
-// LISCENSE: See "LISCENSE.txt" in th root project directory.
-// SUPPORT: See "README.txt" in the root project directory.
+﻿//// Seven
+//// https://github.com/53V3N1X/SevenEngine
+//// LISCENSE: See "LISCENSE.txt" in th root project directory.
+//// SUPPORT: See "README.txt" in the root project directory.
 
-using Seven.Mathematics;
+//using Seven.Mathematics;
 
-namespace SevenEngine.GraphicsCard
-{
-  /// <summary>A delegate for rendering an asset.</summary>
-  public delegate void Render();
-  /// <summary>A delegate for loading an asset (prior to rendering).</summary>
-  public delegate void Load();
-  /// <summary>A delegate for unloading an asset (no longer rendering).</summary>
-  public delegate void Unload();
+//namespace SevenEngine.GraphicsCard
+//{
+//  /// <summary>A delegate for rendering an asset.</summary>
+//  public delegate void Render();
+//  /// <summary>A delegate for loading an asset (prior to rendering).</summary>
+//  public delegate void Load();
+//  /// <summary>A delegate for unloading an asset (no longer rendering).</summary>
+//  public delegate void Unload();
 
-  /// <summary>An abstraction of the graphics library being used.</summary>
-  public static class Graphics
-  {
-    public static void Initialize()
-    {
+//  /// <summary>An abstraction of the graphics library being used.</summary>
+//  public static class Graphics
+//  {
+//    public static void Initialize()
+//    {
 
-    }
+//    }
 
-    public static void SetProjectionMatrix()
-    {
-    }
+//    public static void SetProjectionMatrix()
+//    {
+//    }
 
-    public static void Set
+//    public static void Set
 
-    public Render AbstractRender(
-      float[] vertexBuffer,
-      float[] uvBuffer,
-      float[] normalBuffer,
+//    public Render AbstractRender(
+//      float[] vertexBuffer,
+//      float[] uvBuffer,
+//      float[] normalBuffer,
 
-      )
-    {
-
-
+//      )
+//    {
 
 
-    }
 
 
-  }
-}
+//    }
+
+
+//  }
+//}
